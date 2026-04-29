cdd04758e74e30405927f37881d4479124097e54
XeshSufferer <3a4em_yt@mail.ru>
2026-04-29 03:45:44 +0200

    add gossip and problems fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"QMesh-Sidecar/internal"
+	"QMesh-Sidecar/internal/gossip"
+)
 
 func main() {
-	fmt.Println("Example!")
+	seeds := gossip.SeedsFromEnv()
+	gossipInstance := gossip.NewGossip(seeds)
+
+	if err := gossipInstance.Start(); err != nil {
+		log.Fatalf("Failed to start gossip: %v", err)
+	}
+
+	listener := internal.NewListener(gossipInstance)
+	gossipInstance.SetMemberListener(listener)
+
+	go listener.StartListen()
+
+	fmt.Println("QMesh Sidecar started!")
+	select {}
 }
